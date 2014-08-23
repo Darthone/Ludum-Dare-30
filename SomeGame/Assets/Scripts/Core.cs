bfa637633ca6ad56d8e0a16d78f40eb368eed634
Darthone <dm661@live.com>
2014-08-23 19:04:21 -0400

    increase max core health, add health bar

--- SomeGame/Assets/Scripts/Core.cs
+++ SomeGame/Assets/Scripts/Core.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 
 public class Core : MonoBehaviour {
-    public int coreHealth = 200;
+    public int coreHealth = 300;
+    public Texture2D healthbar;
+    int coreMaxHealth = 300;
+    float barDisplay = 0f;
+    Vector2 pos = Vector2.zero;
+    Vector2 size = new Vector2(100f, 5f);
+
 	// Use this for initialization
 	void Start () {
-	
+        coreMaxHealth = coreHealth;
 	}
 	
 	// Update is called once per frame
@@ -13,11 +19,28 @@ public class Core : MonoBehaviour {
         if (coreHealth <= 0) {
             GameController.control.GameOver();
         }
+        barDisplay = (float)coreHealth / (float)coreMaxHealth;
+        size = new Vector2(barDisplay * 100f, size.y);
 	}
+
+    void OnGUI() {
+        DrawQuad(new Rect(Screen.width / 2 - size.x / 2, Screen.height/2 + 60, size.x, size.y), Color.Lerp(Color.red, Color.green, barDisplay));
+    }
+
+
     void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("EnemyBullet")) {
             coreHealth--;
             Destroy(collision.gameObject);
         }
     }
+
+    void DrawQuad(Rect position, Color color) {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, color);
+        texture.Apply();
+        GUI.skin.box.normal.background = texture;
+        GUI.Box(position, GUIContent.none);
+    }
+
 }
