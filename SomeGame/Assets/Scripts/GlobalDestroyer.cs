7435d227e876ac31ff67e316b819c6aec3490f5e
Darthone <dm661@live.com>
2014-08-22 23:29:58 -0400

    destory on exit

---
+++ SomeGame/Assets/Scripts/GlobalDestroyer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalDestroyer : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (!other.CompareTag("Player"))
+            Destroy(other.gameObject);
+    }
+}
