44f6dd93ed76da8066cc7007f55a5e74397d8359
Darthone <dm661@live.com>
2014-08-23 19:03:13 -0400

    added a speed up power up

--- SomeGame/Assets/Scripts/PowerUp.cs
+++ SomeGame/Assets/Scripts/PowerUp.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PowerUp : MonoBehaviour {
 
-    public enum PowerUpType { Shield = 1, Upgrade, Bomb }
+    public enum PowerUpType { Shield = 1, Upgrade, Bomb, Speed}
     public PowerUpType thisType;
     public GameObject floatingText;
     public int points = 1000;
@@ -12,7 +12,7 @@ public class PowerUp : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	    // choose a type TODO
-        thisType = (PowerUpType)Mathf.Round(Random.Range(1, 3));
+        thisType = (PowerUpType)Mathf.Round(Random.Range(1, 5));
 	}
 
     void OnTriggerEnter2D(Collider2D collision) {
@@ -27,16 +27,23 @@ public class PowerUp : MonoBehaviour {
                 case PowerUpType.Shield:
                     text.guiText.text = "SHIELD";
                     pc.canBeHurt = false;
-                    
                     break;
+
                 case PowerUpType.Upgrade:
                     text.guiText.text = "LASERS +";
                     pc.laserCount++;
+                    pc.shootSpeed -= 0.03f;
+                    pc.laserSpeed++;
                     break;
 
                 case PowerUpType.Bomb:
                     text.guiText.text = "BOMBS +";
                     pc.bombs++;
+                    break;
+
+                case PowerUpType.Speed:
+                    text.guiText.text = "SPEED +";
+                    pc.maxSpeed += 0.25f;
                     break;
             }
             Destroy(this.gameObject);
