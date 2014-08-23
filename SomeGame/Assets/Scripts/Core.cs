bb7c3afae3febd8424124157443ed6cbf02fb8fe
Darthone <dm661@live.com>
2014-08-23 01:57:41 -0400

    code for the core
    enemy ai needs a bit of help

---
+++ SomeGame/Assets/Scripts/Core.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Core : MonoBehaviour {
+    public int coreHealth = 200;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (coreHealth <= 0) {
+            GameController.control.GameOver();
+        }
+	}
+    void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("EnemyBullet")) {
+            coreHealth--;
+            Destroy(collision.gameObject);
+        }
+    }
+}
