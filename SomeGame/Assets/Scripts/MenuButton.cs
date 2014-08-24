9ea1333081c2df331cde38b96d1adfd0ee6c147a
Darthone <dm661@live.com>
2014-08-24 14:43:16 -0400

    menu done!

--- SomeGame/Assets/Scripts/MenuButton.cs
+++ SomeGame/Assets/Scripts/MenuButton.cs
@@ -5,13 +5,20 @@ public class MenuButton : MonoBehaviour {
     public enum ButtonType { Play = 1, Help, Credits }
     public ButtonType thisButton;
     public AudioClip select;
+    public Texture message;
+    public Material mat = null;
 
+    Rect messageRect;
+    Vector2 destination;
+    Vector2 flyoffDest = new Vector2(150f, 500f);
+    float textureWidth;
+    float textureHeight;
     float pushDist = 2f;
-    float startTime;
     Vector3 start;
     bool mouseOver = false;
     bool display = false;
     bool sounded = false;
+    bool flyout;
     BoxCollider2D b;
     Vector2 bs;
     void OnMouseOver() {
@@ -23,22 +30,37 @@ public class MenuButton : MonoBehaviour {
     }
 
     void OnMouseExit() {
+        flyout = true;
         mouseOver = false;
         display = false;
         sounded = false;
     }
 
     void OnMouseUp() {
-        if (mouseOver) {
+        if (mouseOver && !display) {
             switch (thisButton) {
                 case ButtonType.Play:
                     Application.LoadLevel("Game");
                     break;
                 case ButtonType.Help:
+                    //fly in
+                    flyout = false;
+                    display = true;
+                    textureWidth = 600f;
+                    textureHeight = 600f;
+                    messageRect = new Rect(0f, -600f, textureWidth, textureHeight);
+                    destination = new Vector2(150f, 0f);
                     //fly in help
                     break;
                 case ButtonType.Credits:
-                    // roll credits
+                    display = true;
+                    flyout = false;
+                    display = true;
+                    textureWidth = 600f;
+                    textureHeight = 600f;
+                    flyoffDest = new Vector2(150f, -600f);
+                    messageRect = new Rect(150f, 1000f, textureWidth, textureHeight);
+                    destination = new Vector2(150f, -50f);
                     break;
             }
         }
@@ -49,12 +71,28 @@ public class MenuButton : MonoBehaviour {
         bs = b.size;
         start = this.transform.position;
 	}
-	
+    void OnGUI() {
+        if (display || flyout) {
+            Graphics.DrawTexture(messageRect, message, mat);
+        }
+    }
 	// Update is called once per frame
 	void FixedUpdate () {
+        if (display) {
+            if (messageRect.position != destination) {
+                messageRect = new Rect(messageRect.position.x + (destination.x - messageRect.position.x) * 0.1f,  messageRect.position.y + (destination.y - messageRect.position.y) * 0.1f, textureWidth, textureHeight);
+            }
+        }
+        if (flyout) {
+            if (messageRect.position != flyoffDest) {
+                messageRect = new Rect(messageRect.position.x + (flyoffDest.x - messageRect.position.x) * 0.1f, messageRect.position.y + (flyoffDest.y - messageRect.position.y) * 0.1f, textureWidth, textureHeight);
+            } else {
+                flyout = false;
+            }
+        }
+
         if (mouseOver) {
             b.size = bs + new Vector2(pushDist * 10f * 2, 0f);
-            
             this.transform.position = start - new Vector3(pushDist, 0f, 0f);
         } else {
             b.size = bs;
