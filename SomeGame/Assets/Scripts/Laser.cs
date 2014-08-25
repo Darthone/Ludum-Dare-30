e2690ab9710ba17e554c3c20db504aab73cb5f99
Darthone <dm661@live.com>
2014-08-25 00:38:16 -0400

    laser fix

---
+++ SomeGame/Assets/Scripts/Laser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+	
+	// Update is called once per frame
+    void Update() {
+        if (this.gameObject.layer == GameController.control.playerLayer)
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0f);
+        else
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 100f);
+    }
+
+}
